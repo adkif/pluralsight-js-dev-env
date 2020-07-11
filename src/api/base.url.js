8a368bfcf1425dc78e23fc8a8f355535fce406b8
Adolphe Kifungo <akifungo@gmail.com>
2020-07-11 11:31:49 +0200

    demo:toggle mock api

--- src/api/base.url.js
+++ src/api/base.url.js
@@ -1,6 +1,16 @@
 const getBaseUrl = () => {
-    const inDevMode = window.location.hostname == 'localhost';
-    return inDevMode ? 'http://localhost:3001/' : '/';
+    return getQueryStringParameterByName('useMockApi') ? 'http://localhost:3001/' : '/';
+}
+
+const getQueryStringParameterByName = (name, url) => {
+    if (!url) url = window.location.href;
+    // eslint-disable-next-line no-useless-escape
+    name = name.replace(/[\[\]]/g, "\\$&");
+    const regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
 }
 
 module.exports = getBaseUrl;
