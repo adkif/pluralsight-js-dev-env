3d93959452045ae1220ab96e8f591717cd438357
Adolphe Kifungo <akifungo@gmail.com>
2020-07-11 13:23:21 +0200

    demo:cache busting

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -2,6 +2,7 @@
 import path from 'path';
 import UglifyJsPlugin from 'uglifyjs-webpack-plugin';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
+import WebpackMd5Hash from 'webpack-md5-hash';
 
 export default {
     devtool: 'source-map',
@@ -14,9 +15,10 @@ export default {
     output: {
         path: path.resolve(__dirname, 'dist'),
         publicPath: '/',
-        filename: '[name].js'
+        filename: '[name].[chunkhash].js'
     },
     plugins: [
+        new WebpackMd5Hash(),
         new HtmlWebpackPlugin({
             template: 'src/index.html',
             minify: {
